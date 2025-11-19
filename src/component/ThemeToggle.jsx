8c69250b7eea28d95781b2fe7110bdeef0576fb7
yugoo90 <uosuji54@gmail.com>
2025-11-18 18:55:58 -0700

    removed the hidden class themetoggle button for small screens

--- src/component/ThemeToggle.jsx
+++ src/component/ThemeToggle.jsx
@@ -29,7 +29,7 @@ export const ThemeToggle = () => {
     }
 
     return (
-        <button onClick={toggleTheme} className={cn("fixed max-sm:hidden top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", "focus:outline-hidden")}>
+        <button onClick={toggleTheme} className={cn("fixed top-5 right-5 z-50 p-2 rounded-full transition-colors duration-300", "focus:outline-hidden")}>
 
             {isDarkMode ? (
                 <Sun className="h-6 w-6 text-yellow-300" />
